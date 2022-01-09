24fc398ffd5592b8a815a8e474a9c5bc547a0137
Alexander Stanovoy <alex.stanovoy@gmail.com>
2022-01-10 01:24:42 +0300

    pip install

--- grade.sh
+++ grade.sh
@@ -3,8 +3,12 @@
 echo "[INFO]   PROBLEM_NAME=\"$PROBLEM_NAME\""
 echo "[INFO]   PROBLEM_SOLUTION=\"$PROBLEM_SOLUTION\""
 echo "[INFO]   PROBLEM_ROOT=\"$PROBLEM_ROOT\""
+echo "[INFO]   USER_LOGIN=\"$USER_LOGIN\""
 
 ./ci/scripts/file-routine.sh || exit 1
 ./ci/scripts/setup-deps.sh || exit 1
 ./ci/scripts/test-solution.sh || exit 1
+
+pip install gspread itertools oauth2client || exit 1
+echo "$GSHEET_API_TOKEN" > ci/scripts/token.json || exit 1
 ./ci/scripts/submit-result.py || exit 1
